5c14522a64467fb43540aeea9323282ae57921ad
tommasoamico <tommaso99.amico@gmail.com>
2022-06-04 22:01:31 +0200

    Changed the return of the cumulative.cumulative function

--- cumulative.py
+++ cumulative.py
@@ -16,7 +16,7 @@ def cumulative(array, title='Cumulative', xlabel='x axis', ylabel='y axis'):
     ax.set_ylabel(ylabel)
     ax.minorticks_on()
     ax.scatter(array, cumul, s=150, edgecolor='black', alpha=0.7)
-    return fig.tight_layout(), ax, cumul, array
+    return fig, ax, cumul, array
 
 
 
@@ -69,12 +69,16 @@ def info_cumulative_data():
 
 def cumulative_matrix(array, densities, title='Cumulative', xlabel='x axis', ylabel='y axis', log_log = True, fontsize_title = 20, fontsize_x = 15, fontsize_y = 15, offset_x = 14, offset_y = 14, s=250, legend_size=14, unit_of_measure='$\\mu M$', k_round=2):
     fig, ax = plt.subplots(1,1,figsize=(15,10))
+    arrays = []
+    cumulatives = []
     for column, density in zip(range(array.shape[1]), densities):
         arr = array[:,column]
         arr = np.array(arr)
         arr = np.sort(arr)
         arr = arr[~np.isnan(arr)]
+        arrays.append(arr)
         cumul = 1 - np.arange(0, len(arr))/(len(arr))
+        cumulatives.append(cumul)
         if log_log == True:
             ax.set_xscale('log')
             ax.set_yscale('log')
@@ -93,7 +97,7 @@ def cumulative_matrix(array, densities, title='Cumulative', xlabel='x axis', yla
         ax.yaxis.set_tick_params(labelsize=15, size=10)
         ax.yaxis.set_tick_params(which='minor', size=4)
         ax.legend(shadow = True, framealpha=1, facecolor='aliceblue', edgecolor='black',  prop={'weight':'bold','size':legend_size}, loc='best')
-    return fig.tight_layout(), ax
+    return fig, ax, arrays, cumulatives
 
 
 
@@ -127,7 +131,7 @@ def cumulative_collapse(array, alpha, phi, densities, title='Cumulative', xlabel
         ax.yaxis.set_tick_params(labelsize=15, size=10)
         ax.yaxis.set_tick_params(which='minor', size=4)
         ax.legend(shadow=True, framealpha=1, facecolor='aliceblue', edgecolor='black',  prop={'weight':'bold','size':legend_size}, loc='best')
-    return fig.tight_layout(), ax
+    return fig, ax
 
 
 
